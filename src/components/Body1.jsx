50afe52c3def2572ec9cb80fbf7bc8bc24a8d05e
Muhammad Ahmad Fridi <muhammadahmadfridi71al@gmail.com>
2025-07-28 12:46:32 +0500

    Update Body1.jsx

--- src/components/Body1.jsx
+++ src/components/Body1.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import img22 from "../images/ahmad.png" 
+import img22 from "../images/ahmad.png.jpg" 
 
 const HeroSection = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
