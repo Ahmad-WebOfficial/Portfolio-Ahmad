ffa71c18915b72145a0ece9ee01e23e2784b621e
Muhammad Ahmad Fridi <muhammadahmadfridi71al@gmail.com>
2025-08-01 12:17:36 +0500

    Update View.jsx

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -26,21 +26,18 @@ const ProjectManager = () => {
       </div>
 
       <div className="h-full bg-[#1a282e] flex flex-col mt-15 items-center p-5 md:p-10 text-white">
-        <img
-          src={img1}
-          alt="Muhammad Ahmad Fridi"
-          className="w-40 h-40 md:w-48 md:h-48 rounded-full object-contain mb-4 shadow-lg border-4 border-white"
-        />
+        
+        
 
         <h1 className="text-3xl md:text-4xl font-bold mb-3">
           Muhammad Ahmad Fridi
         </h1>
 
         <p className="text-center text-gray-300 max-w-2xl mb-10">
-          I am Muhammad Ahmad Fridi, a passionate Frontend Developer dedicated
+          I am Muhammad Ahmad Fridi, a passionate Fullsatck Developer dedicated
           to building modern, responsive, and user-friendly websites. I have
           successfully completed multiple projects in web development using
-          React, Tailwind CSS, and JavaScript. Many more projects are in
+          React, Tailwind CSS, and JavaScript Nodejs and MongoDB. Many more projects are in
           progress, as I continue to learn and grow in this fast-moving tech
           world. My mission is to create impactful digital experiences that
           solve real-world problems.
